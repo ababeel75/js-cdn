f6dc947b56da7f21f43158b990a7928318d28e20
ababeel75 <131642315+ababeel75@users.noreply.github.com>
2024-11-12 14:58:50 +0500

    Update page-script.js

--- page-script.js
+++ page-script.js
@@ -1,5 +1,5 @@
 // Select the button with id "myButton"
 document.getElementById("myButton").addEventListener("click", function () {
     // Display an alert message when the button is clicked
-    alert("Button was clicked!");
+    alert("Button was not clicked!");
 })
